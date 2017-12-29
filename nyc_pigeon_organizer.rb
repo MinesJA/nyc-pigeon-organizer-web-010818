9421bf0cce744873ed5c2c39606228d6b3e85741
MinesJA <minesja@gmail.com>
2017-12-29 08:25:01 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+
+  data.each do |label, p_hash|
+    p_hash.each do |attribute, attribute_pigeons|
+      attribute_pigeons.each do |pigeon|
+
+        hash[pigeon] ||= {}
+        hash[pigeon][label] ||= []
+        hash[pigeon][label] << attribute.to_s
+      end
+    end
+  end
+  hash
 end
+
+
+
+
+
+#label = :color
+
+#p_hash =
+# {:purple=>["Theo", "Peter Jr.", "Lucky"],
+# :grey=>["Theo", "Peter Jr.", "Ms. K"],
+# :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
+
+# :brown=>["Queenie", "Alex"]}
+
+#attribute = :purple
+
+#attribute_pigeons = ["Theo", "Peter Jr.", "Lucky"]
+
+#pigeons = "Theo"
+
+
+
+=begin
+        if hash.include?(pigeons)
+          hash[pigeons][color_label] << color
+        else
+
+
+        if hash.include?(pigeons)
+
+        else
+          hash[pigeons] = {color_label=>[color.to_s]}
+=end
